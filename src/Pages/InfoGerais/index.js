4cc5c74034c2e63817410401612c9eed0c2f63cb
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-04-27 16:09:15 -0300

    add card Cruzamento de dados

--- src/Pages/InfoGerais/index.js
+++ src/Pages/InfoGerais/index.js
@@ -38,6 +38,23 @@ export default function () {
                             <div className="col-md-8">
                                 <div className="card-body" id="normas-diretrizes">
                                     <a href="/treinamentogalpao">
+                                        <h5 className="card-title text-center">Dep. Cruzamentos de Dados</h5>
+                                    </a>
+                                    <p className="card-text">Neste card você encontrará todos os processos relacionados ao respectivo setor.</p>
+                                    <p className="card-text"><small className="text-muted">Last updated 25-04-2023</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card mb-3 card-info card-style">
+                        <div className="row div-column g-0">
+                            <div className="div-img col-md-4">
+                                <img src="./assets/img/logo.svg" className="img-fluid rounded-start" alt="..." />
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body" id="normas-diretrizes">
+                                    <a href="/treinamentogalpao">
                                         <h5 className="card-title text-center">Dep. Financeiro e Compras</h5>
                                     </a>
                                     <p className="card-text">Neste card você encontrará todos os processos relacionados ao respectivo setor.</p>

63a0bc714165e2bf9eeb95f9a3b251cc5217f8e1
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-24 09:29:54 -0300

    add mod19 card

--- src/Pages/InfoGerais/index.js
+++ src/Pages/InfoGerais/index.js
@@ -141,6 +141,24 @@ export default function () {
                     <div className="card mb-3 card-info card-style">
                         <div className="row div-column g-0">
                             <div className="div-img col-md-4">
+                                <img src="./assets/img/logo.svg" className="img-fluid rounded-start" alt="..." />
+                            </div>
+                            <div className="col-md-8">
+                                <div className="card-body" id="normas-diretrizes">
+                                    <a href="/mod19">
+                                        <h5 className="card-title text-center">Mod 19</h5>
+                                    </a>
+
+                                    <p className="card-text">Modelo 19 saiba o que é e como preenchê-lo corretamente no link acima.</p>
+                                    <p className="card-text"><small className="text-muted">Last updated: 14-03-2023</small></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="card mb-3 card-info card-style">
+                        <div className="row div-column g-0">
+                            <div className="div-img col-md-4">
                                 <img src="./assets/img/acuttis.svg" className="img-fluid rounded-start" alt="..." />
                             </div>
                             <div className="col-md-8">

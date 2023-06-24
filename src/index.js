d3d8d2b9bab9175d7bee74d1795ed7accb220809
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-24 09:25:39 -0300

    add routes and m19page

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ import Header from "./Components/Header";
 import Contacts from "./Components/Contacts";
 import Footer from "./Components/Footer";
 import InfoGerais from "./Pages/InfoGerais";
+import Mod19 from "./Pages/Mod19";
+import DuvidasAcuttis from "./Pages/DuvidasAcuttis";
+import AssEmail from "./Pages/Email";
+import ContaMicrosoft from "./Pages/ContaMicrosoft";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,6 +21,10 @@ root.render(
 
         <Routes>
             <Route path="/" element={<InfoGerais />} />
+            <Route path="/mod19" element={<Mod19 />} />
+            <Route path="/duvidasaccutis" element={<DuvidasAcuttis />} />
+            <Route path="/assinatura_e-mail" element={<AssEmail />} />
+            <Route path="/conta_microsoft" element={<ContaMicrosoft />} />
         </Routes>
 
         <Contacts />
